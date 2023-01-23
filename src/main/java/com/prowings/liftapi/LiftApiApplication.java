9469a342058db53b00daa8f6b2298ba1e311773a
devNishigandhaPatil <nishigandhapatil2805.com>
2023-01-24 00:15:42 +0530

    first commit

--- src/main/java/com/prowings/liftapi/LiftApiApplication.java
+++ src/main/java/com/prowings/liftapi/LiftApiApplication.java
@@ -13,20 +13,21 @@ public class LiftApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LiftApiApplication.class, args);
 		Scanner sc = new Scanner(System.in);
-		Set<Integer> s = new HashSet();
+		Set<Integer> selectedFloors = new HashSet();
 		System.out.println("*****Lift_Travels*****");
 		System.out.println("Enter CurrentFloor");
 		int currentFloor = sc.nextInt();
 		System.out.println("Enter 1 For Upward Direction OR Enter -1 For DownWard Direction");
 		int direction = sc.nextInt();
 		System.out.println("Due To Load  Issue Enter Only  5 Floors ");
-		s.add(sc.nextInt());
-		s.add(sc.nextInt());
-		s.add(sc.nextInt());
-		s.add(sc.nextInt());
-		s.add(sc.nextInt());
+		selectedFloors.add(sc.nextInt());
+		selectedFloors.add(sc.nextInt());
+		selectedFloors.add(sc.nextInt());
+		selectedFloors.add(sc.nextInt());
+		selectedFloors.add(sc.nextInt());
 
-		System.out.println("Seq. By Which Lift Travels :=" + Lift_Travels.liftTravel(currentFloor, direction, s));
+		System.out.println(
+				"Seq. By Which Lift Travels :=" + Lift_Travels.liftTravel(currentFloor, direction, selectedFloors));
 
 	}
 
